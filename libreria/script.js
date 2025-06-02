09f3941d53f471a4d4b699e03fb9dab335bf9b86
Jaime <jaimezpe.android@gmail.com>
2025-06-02 18:39:38 +0200

    Changes in description
    - Legal por si acaso en 404, login, matematicas, waitlist, index
    - Nuevo foro (el problema es que es con github la cuenta)
    - Archivos por rellenar de legal
    - Nueva libreria y copia de la anterior
    - Planes de pago por si acaso

---
+++ libreria/script.js
@@ -0,0 +1,270 @@
+const librosData = [
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 2,
+    title: "Algoritmos Avanzados",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2023-12-15",
+    autor: "Juan Pérez",
+    pdf: "https://example.com/algoritmos_avanzados.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  },
+  
+  {
+    id: 1,
+    title: "Introducción a la programación",
+    thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/youtube-thumbnail-retrowave-design-template-facd275829297c4a471f2f1af6436508_screen.jpg?ts=1561496227",
+    fecha: "2024-05-01",
+    autor: "María González",
+    pdf: "https://example.com/introduccion_programacion.pdf"
+  }
+];
+
+let ordenDescendente = true;
+
+const container = document.getElementById("librosContainer");
+const ordenarBtn = document.getElementById("ordenarBtn");
+const textoOrdenar = document.getElementById("buscarlibro-texto-ordenar");
+
+function renderLibros(libros) {
+  container.innerHTML = "";
+  libros.forEach(libro => {
+    const div = document.createElement("div");
+    div.className = "libro";
+
+    div.innerHTML = `
+      <img src="${libro.thumbnail}" alt="${libro.title}">
+      <div class="libro-info">
+        <h3>${libro.title}</h3>
+        <p><strong>Autor:</strong> ${libro.autor}</p>
+        <p><strong>Fecha:</strong> ${libro.fecha}</p>
+        <a href="${libro.pdf}" target="_blank">Ver PDF</a>
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+function ordenarPorFecha() {
+  ordenDescendente = !ordenDescendente;
+  const librosOrdenados = [...librosData].sort((a, b) => {
+    return ordenDescendente
+      ? new Date(b.fecha) - new Date(a.fecha)
+      : new Date(a.fecha) - new Date(b.fecha);
+  });
+
+  textoOrdenar.textContent = ordenDescendente ? "Más recientes" : "Más antiguos";
+  renderLibros(librosOrdenados);
+}
+
+ordenarBtn.addEventListener("click", ordenarPorFecha);
+
+// Inicial
+ordenarPorFecha();
